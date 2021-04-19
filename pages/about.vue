4d28168871280639ff901e3897f0e7029b5ec6c8
blacksonic <soos.gabor86@gmail.com>
2021-04-19 19:46:17 +0200

    abot section

--- pages/about.vue
+++ pages/about.vue
@@ -14,13 +14,41 @@
           />
         </figure>
 
+        <p><b>Hi, I'm Gabor!</b></p>
+        I'm a technical lead, software engineer and content creator. I was born
+        in 1986 in Hungary. After graduating with Master's degree in Electrical
+        <a
+          href="Engineering at Budapest University of Technology and Economics"
+        >
+          https://www.bme.hu/?language=en
+        </a>
+        I've turned towards software development.
+        <p></p>
+
         <p>
-          I have started my career as a web developer 10+ years ago. I like
-          challenges in which I can use modern tools and technologies to develop
-          outstanding web applications in look, performance and efficiency. I
-          specialise in front-end web technologies - Vue, React, and Angular. My
-          back-end stack includes Node.js with Express/Koa, and PHP. I do most
-          of my projects with Test Driven Development.
+          I've written code for
+          <a href="https://www.doclerholding.com/en/main/">Docler Holding</a>,
+          <a href="https://emarsys.com/">Emarsys</a>, currently
+          <a href="https://www.sap.com/index.html">SAP</a> and in my free time
+          for the
+          <a href="https://github.com/vuesomedev">open source community</a>.
+          I've started as PHP developer mainly focused in backend technologies
+          and fell in love with Javascript at my current workplace. Since then
+          there is no turning back: Javascript development takes up most of
+          coding time.
+        </p>
+
+        <p>
+          During my work I like challenges in which I can use modern tools and
+          technologies to develop outstanding web applications in look,
+          performance and efficiency. I specialise in front-end web technologies
+          - Vue, React, and Angular. My back-end stack includes Node.js with
+          Express/Koa. I do most of my projects with Test Driven Development.
+        </p>
+
+        <p>
+          I spend most of my free-time with my dog Mignon 🐶 teaching her new
+          tricks.
         </p>
       </div>
     </article>
